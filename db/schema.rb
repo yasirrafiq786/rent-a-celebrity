1fc49fd0024a5a8214462b0c3da01f9d2138293f
Sonya Duke <sonya.j.duke@gmail.com>
2020-05-16 15:40:07 +1000

    removed map from celebrity_detailed_card

--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,22 @@ ActiveRecord::Schema.define(version: 2020_05_16_042256) do
     t.index ["user_id"], name: "index_celebrities_on_user_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_05_16_042256) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -80,4 +97,6 @@ ActiveRecord::Schema.define(version: 2020_05_16_042256) do
   add_foreign_key "bookings", "celebrities"
   add_foreign_key "bookings", "users"
   add_foreign_key "celebrities", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
