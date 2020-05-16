a759ece78ea020d57684a825227f6814aa2ed446
Markus Sachinidis <m.sachinidis@protonmail.com>
2020-05-16 14:49:50 +1000

    added table for bookings page - need to change to update as div cards

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:show, :edit]
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
