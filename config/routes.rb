8590379b277519dcd7133f12c6ce92bf5cca5ac5
Andy Leong <mindreko@gmail.com>
2020-05-19 19:52:48 +1000

    Routes for messaging complete

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:show, :edit]
+  resources :bookings, only: [:show, :edit] do 
+    resources :messages, only: [:index, :create]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "making a chatroom called master"
+master = Chatroom.create(name: "master")
+puts "chatroom master done"
+
